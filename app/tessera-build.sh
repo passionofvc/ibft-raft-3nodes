d577504f8302c0b918f0dfa9d3dc57d27e2a45b8
passionofvc <yamada.passion@gmail.com>
2020-09-06 19:26:03 +0900

    direct use tessera jar

--- app/tessera-build.sh
+++ app/tessera-build.sh
@@ -5,9 +5,9 @@
 #mvn install -Dmaven.test.skip=true
 
 #download tessera from git release
-cd $NODE_HOME/app
-mkdir -p tessera && cd tessera
-wget https://oss.sonatype.org/service/local/repositories/releases/content/com/jpmorgan/quorum/tessera-app/0.10.6/tessera-app-0.10.6-app.jar
+#cd $NODE_HOME/app
+#mkdir -p tessera && cd tessera
+#wget https://oss.sonatype.org/service/local/repositories/releases/content/com/jpmorgan/quorum/tessera-app/0.10.6/tessera-app-0.10.6-app.jar
+#echo 'export TESSERA_JAR=$NODE_HOME/app/tessera/tessera-app-0.10.6-app.jar' >> ~/.bashrc
+
 echo 'export TESSERA_JAR=$NODE_HOME/app/tessera/tessera-app-0.10.6-app.jar' >> ~/.bashrc
-
-
