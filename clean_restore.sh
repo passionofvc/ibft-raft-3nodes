b000802f89925403cdf68470fa63c87bb288ca0a
passionofvc <yamada.passion@gmail.com>
2020-09-07 17:58:09 +0900

    add restore file

--- clean_restore.sh
+++ clean_restore.sh
@@ -1,4 +1,4 @@
-git restore genesis.json start-permission.sh
+git restore genesis.json permissioned-nodes.json start-permission.sh
 rm -rf  keys/key* keys/tm*
 rm -rf output nohup.out raft/nodekey*
 rm -rf qdata start-permission.sh.bak
