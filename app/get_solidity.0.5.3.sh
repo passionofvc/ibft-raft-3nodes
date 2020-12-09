297b5db78f51219b4c17d793e90d0e502e5e6f52
passionofvc <yamada.passion@gmail.com>
2020-12-09 12:58:40 +0900

    Update get_solidity.0.5.3.sh

--- app/get_solidity.0.5.3.sh
+++ app/get_solidity.0.5.3.sh
@@ -1,6 +1,6 @@
 # solidity.0.5.3
+#download solidity from git release
 
-download solidity from git release
 cd $NODE_HOME/app
 mkdir -p solidity.0.5.3 && cd solidity.0.5.3
 wget "https://github.com/ethereum/solidity/releases/download/v0.5.3/solc-static-linux" -O solc
