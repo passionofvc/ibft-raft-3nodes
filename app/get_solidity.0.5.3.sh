f5e36eb4a93f28fcd27dd4860c70fde6228fc40a
passionofvc <yamada.passion@gmail.com>
2020-12-09 12:59:19 +0900

    Update get_solidity.0.5.3.sh

--- app/get_solidity.0.5.3.sh
+++ app/get_solidity.0.5.3.sh
@@ -4,4 +4,5 @@
 cd $NODE_HOME/app
 mkdir -p solidity.0.5.3 && cd solidity.0.5.3
 wget "https://github.com/ethereum/solidity/releases/download/v0.5.3/solc-static-linux" -O solc
+chmod u+x solc
 SOLIDITY_DIR=`pwd`; echo "export PATH=$SOLIDITY_DIR:\$PATH" >> ~/.bashrc
