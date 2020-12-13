8ad004f3f0216f41f2ee09dc65f68ac5dc3325d1
passionofvc <yamada.passion@gmail.com>
2020-12-13 19:10:26 +0900

    add check

---
+++ check.sh
@@ -0,0 +1,8 @@
+cat keys/key1  | jq .address
+cat keys/key2  | jq .address
+cat keys/key3  | jq .address
+
+cat qdata/dd1/permission-config.json  | jq .accounts
+cat qdata/dd2/permission-config.json  | jq .accounts
+cat qdata/dd3/permission-config.json  | jq .accounts
+
