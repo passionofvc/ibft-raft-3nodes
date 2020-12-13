f64865c699a747b7514f523405720bd8c7b4ca62
passionofvc <yamada.passion@gmail.com>
2020-12-13 15:23:39 +0900

    add solc

--- app/get_solidity.0.5.3.sh
+++ app/get_solidity.0.5.3.sh
@@ -1,8 +1,16 @@
 # solidity.0.5.3
 #download solidity from git release
 
-cd $NODE_HOME/app
-mkdir -p solidity.0.5.3 && cd solidity.0.5.3
-wget "https://github.com/ethereum/solidity/releases/download/v0.5.3/solc-static-linux" -O solc
-chmod u+x solc
-SOLIDITY_DIR=`pwd`; echo "export PATH=$SOLIDITY_DIR:\$PATH" >> ~/.bashrc
+#cd $NODE_HOME/app
+#mkdir -p solidity.0.5.3 && cd solidity.0.5.3
+#wget "https://github.com/ethereum/solidity/releases/download/v0.5.3/solc-static-linux" -O solc
+#chmod u+x solc
+
+OS=$(uname -s)
+SOLIDITY_DIR=
+if [[ ${OS} == "Darwin" ]] ; then
+    SOLIDITY_DIR="$PWD/solidity.0.5.3/mac"
+elif  [[ ${OS} == "Linux" ]] ; then
+    SOLIDITY_DIR="$PWD/solidity.0.5.3/linux"
+fi
+echo "export PATH=$SOLIDITY_DIR:\$PATH" >> ~/.bashrc
