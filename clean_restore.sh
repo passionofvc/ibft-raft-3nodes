0e7f6ee6e7c02c2e3e6f7e93e82eede0cf1cd069
passionofvc <yamada.passion@gmail.com>
2020-12-13 16:22:05 +0900

    fix clean

--- clean_restore.sh
+++ clean_restore.sh
@@ -1,10 +1,3 @@
-git restore genesis.json permissioned-nodes.json start-permission.sh
-rm -rf  keys/key* keys/tm*
-rm -rf output nohup.out raft/nodekey*
-rm -rf qdata start-permission.sh.bak
-rm -rf static-nodes.json
-
-
-
-
+git reset --hard
+git clean -fd
 
