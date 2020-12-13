df8fd7bc51cedc6e7d002fd95b4537d4ecc538fe
passionofvc <yamada.passion@gmail.com>
2020-12-13 16:54:35 +0900

    add istabul module

--- app/istanbul-tools-build.sh
+++ app/istanbul-tools-build.sh
@@ -1,10 +1,18 @@
 #go1.14.5以上
 
-cd $NODE_HOME/app
-rm -rf istanbul-tools 1>/dev/null 2>&1
-git clone https://github.com/passionofvc/istanbul-tools.git
-cd istanbul-tools
-make
+#cd $NODE_HOME/app
+#rm -rf istanbul-tools 1>/dev/null 2>&1
+#git clone https://github.com/passionofvc/istanbul-tools.git
+#cd istanbul-tools
+#make
 
-echo 'export PATH=$NODE_HOME/app/istanbul-tools/build/bin:$PATH' >> ~/.bashrc
+OS=$(uname -s)
+ISTANBUL_DIR=
+if [[ ${OS} == "Darwin" ]] ; then
+    ISTANBUL_DIR="\$NODE_HOME/app/istanbul-tools/mac/build/bin"
+elif  [[ ${OS} == "Linux" ]] ; then
+    ISTANBUL_DIR="\$NODE_HOME/app/istanbul-tools/linux/build/bin"
+fi
+
+echo 'export PATH=$ISTANBUL_DIR:$PATH' >> ~/.bashrc
 
