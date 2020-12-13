26a80b3c4d46b9b30af5b9f02fb40867d46c21d8
passionofvc <yamada.passion@gmail.com>
2020-12-13 20:41:16 +0900

    add check

--- check.sh
+++ check.sh
@@ -6,3 +6,15 @@ cat qdata/dd1/permission-config.json  | jq .accounts
 cat qdata/dd2/permission-config.json  | jq .accounts
 cat qdata/dd3/permission-config.json  | jq .accounts
 
+data=$(cat istanbul-genesis.json | jq -r .extraData)
+istanbul extra decode --extradata ${data}
+
+n1=$(cat qdata/dd1/geth/nodekey)
+n2=$(cat qdata/dd2/geth/nodekey)
+n3=$(cat qdata/dd3/geth/nodekey)
+istanbul address --nodekeyhex ${n1}
+istanbul address --nodekeyhex ${n2}
+istanbul address --nodekeyhex ${n3}
+
+
+
